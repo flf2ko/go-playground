c4f2e4dd771efac587957e0cf5872d74d9c37c37
flf2ko <noobfriend@gmail.com>
2025-09-09 17:59:16 +0800

    add alloy & pyroscope

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
 	"os/signal"
@@ -8,8 +9,25 @@ import (
 
 	"github.com/flf2ko/playground/go-api-sample/database"
 	"github.com/flf2ko/playground/go-api-sample/handlers"
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"go.opentelemetry.io/contrib/detectors/aws/ecs"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	sdklog "go.opentelemetry.io/otel/sdk/log"
+	sdkmetric "go.opentelemetry.io/otel/sdk/metric"
+	sdkresource "go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+
+	_ "net/http/pprof"
+
+	_ "github.com/grafana/pyroscope-go/godeltaprof/http/pprof"
 )
 
 func main() {
@@ -55,6 +73,14 @@ func setupRouter(linkHandler *handlers.LinkHandler) *gin.Engine {
 
 	router := gin.Default()
 	router.ContextWithFallback = true
+	pprof.Register(router)
+
+	lp, tp, mp, _ := setupOTEL()
+	defer func() {
+		_ = tp.Shutdown(context.Background())
+		_ = lp.Shutdown(context.Background())
+		_ = mp.Shutdown(context.Background())
+	}()
 
 	// Add CORS middleware
 	router.Use(func(c *gin.Context) {
@@ -110,4 +136,114 @@ func getEnv(key, defaultValue string) string {
 		return value
 	}
 	return defaultValue
+}
+
+func setupOTEL() (*sdktrace.TracerProvider, *sdklog.LoggerProvider, *sdkmetric.MeterProvider, error) {
+	tp, err := initTracerProvider()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	lp, err := initLoggerProvider()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	mp, err := initMeterProvider()
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	return tp, lp, mp, nil
+}
+
+func initTracerProvider() (*sdktrace.TracerProvider, error) {
+	ctx := context.Background()
+
+	exporter, err := otlptrace.New(ctx, otlptracegrpc.NewClient())
+	if err != nil {
+		log.Fatalf("new otlp trace grpc exporter failed: %v", err)
+		return nil, err
+	}
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(initResource()),
+	)
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(propagation.TraceContext{}, propagation.Baggage{}))
+	return tp, nil
+}
+
+func initMeterProvider() (*sdkmetric.MeterProvider, error) {
+	ctx := context.Background()
+
+	exporter, err := otlpmetricgrpc.New(ctx)
+	if err != nil {
+		log.Fatalf("new otlp metric grpc exporter failed: %v", err)
+		return nil, err
+	}
+
+	mp := sdkmetric.NewMeterProvider(
+		sdkmetric.WithReader(sdkmetric.NewPeriodicReader(exporter)),
+		sdkmetric.WithResource(initResource()),
+	)
+	otel.SetMeterProvider(mp)
+	return mp, nil
+}
+
+func initLoggerProvider() (*sdklog.LoggerProvider, error) {
+	// tell loki to use host.name and cloud.region as labels
+	lokiHint := attribute.KeyValue{
+		Key: attribute.Key("loki.resource.labels"),
+		Value: attribute.StringSliceValue([]string{
+			string(semconv.CloudRegionKey.String("").Key),
+			string(semconv.ServiceNameKey.String("").Key),
+			string(semconv.HostName("").Key),
+		}),
+	}
+
+	lp := sdklog.NewLoggerProvider(
+		// sdklog.WithBatcher(exporter),
+		sdklog.WithResource(newResource(lokiHint)),
+	)
+
+	// Log.logger = loggerProvider.Logger(
+	// 	"ride-share",
+	// 	logs.WithInstrumentationVersion("0.0.1"),
+	// 	logs.WithSchemaURL(semconv.SchemaURL),
+	// )
+
+	return lp, nil
+}
+
+func initResource() (resource *sdkresource.Resource) {
+	extraResources, _ := sdkresource.New(
+		context.Background(),
+		sdkresource.WithOS(),
+		sdkresource.WithProcess(),
+		sdkresource.WithContainer(),
+		sdkresource.WithHost(),
+		sdkresource.WithDetectors(
+			ecs.NewResourceDetector(),
+		),
+	)
+	resource, _ = sdkresource.Merge(
+		sdkresource.Default(),
+		extraResources,
+	)
+
+	return
+}
+
+func newResource(extraAttrs ...attribute.KeyValue) *sdkresource.Resource {
+	host, _ := os.Hostname()
+
+	attrs := append([]attribute.KeyValue{
+		semconv.ServiceNameKey.String("go-playground"),
+		// semconv.CloudRegionKey.String(os.Getenv("REGION")),
+		semconv.HostName(host),
+	}, extraAttrs...)
+	return sdkresource.NewWithAttributes(
+		semconv.SchemaURL,
+		// Note that ServiceNameKey attribute can include chars not allowed in Pyroscope
+		// application name, therefore it should be used carefully.
+		attrs...,
+	)
 }
